8440875f701a4eff80cc7a394ca2d49991390085
Mamadou <korka25@live.com>
2021-06-26 21:44:52 +0200

    deploy

---
+++ precache-manifest.aa8b0ac18848d5d88b007c09059082d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe4a18e47292ea82171",
+    "url": "/pomodoro-app/css/app.228d86f3.css"
+  },
+  {
+    "revision": "8c13943630294f0592c98c2c3557359e",
+    "url": "/pomodoro-app/index.html"
+  },
+  {
+    "revision": "afe4a18e47292ea82171",
+    "url": "/pomodoro-app/js/app.a2c0edc0.js"
+  },
+  {
+    "revision": "bd82bf0a8f4bac42df5b",
+    "url": "/pomodoro-app/js/chunk-vendors.d5f5d4a6.js"
+  },
+  {
+    "revision": "9740897aba3968fbf2c400b4f79fa094",
+    "url": "/pomodoro-app/manifest.json"
+  },
+  {
+    "revision": "2a908facef8cbb45c825ce866cbc027b",
+    "url": "/pomodoro-app/media/kichen-timer.2a908fac.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pomodoro-app/robots.txt"
+  }
+]);
